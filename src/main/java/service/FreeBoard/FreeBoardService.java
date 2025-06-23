30ebe70f5285ff94b533774dba9981fec204be35
DongHyun_Lee <58761373+sam3319@users.noreply.github.com>
2025-06-24 03:55:44 +0900

    Delete src/main directory

--- src/main/java/service/FreeBoard/FreeBoardService.java
+++
@@ -1,8 +0,0 @@
-package service.FreeBoard;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface FreeBoardService {
-	public void execute(HttpServletRequest request, HttpServletResponse response);
-}
--- src/main/java/service/ReviewBoard/ReviewBoardService.java
+++
@@ -1,8 +0,0 @@
-package service.ReviewBoard;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface ReviewBoardService {
-	public void execute(HttpServletRequest request, HttpServletResponse response);
-}
